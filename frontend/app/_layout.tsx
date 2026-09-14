bcd26bec7802dd294b7b7b32692e4126156d1b81
MrPee309 <linecker309@yahoo.com>
2026-09-14 12:06:00 -0400

    Add browse-businesses screen to layout

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -83,6 +83,7 @@ function RootNavigator() {
         <Stack.Screen name="product-details" />
         <Stack.Screen name="browse-technicians" />
         <Stack.Screen name="transport-category" />
+        <Stack.Screen name="browse-businesses" />
         <Stack.Screen name="search-results" />
         <Stack.Screen name="favorites" />
         <Stack.Screen name="make-a-demand" />
